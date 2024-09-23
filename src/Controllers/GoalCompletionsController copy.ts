1b97de9b699bceb845a41ab9faccf6a4084e81ef
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-23 09:52:28 -0300

    one compeltio count goasl  lenght

--- src/Controllers/GoalCompletionsController copy.ts
+++ src/Controllers/GoalCompletionsController copy.ts
@@ -1,53 +1,51 @@
-import { Request, Response } from "express";
-import {PrismaClient} from '@prisma/client'
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function createGoalsCompletions(req: Request, res: Response){
-    try {
-        const goals = await prisma.goal_Completions.create({
-            data: {
-                goalId: req.body.goalId,
-            }
-        })
-        
-        return res.status(201).send({msg: "Sucesso!", goals})
-    } catch (error) {
+async function createGoalsCompletions(req: Request, res: Response) {
+  try {
+    const goals = await prisma.goal_Completions.create({
+      data: {
+        goalId: req.body.goalId,
+      },
+    })
 
-        return res.status(400).send({msg: "ERROR!", error})
-
-        
-    }
+    return res.status(201).send({ msg: 'Sucesso!', goals })
+  } catch (error) {
+    return res.status(400).send({ msg: 'ERROR!', error })
+  }
 }
 
+async function getGoalsCompletions(req: Request, res: Response) {
+  try {
+    const goals = await prisma.goal_Completions.findMany()
 
-async function getGoalsCompletions(req: Request, res: Response){
-    try {
-        const goals = await prisma.goal_Completions.findMany()
-        
-        return res.status(201).send( goals)
-    } catch (error) {
-
-        return res.status(400).send({msg: "ERROR!", error})
-
-        
-    }
+    return res.status(201).send(goals)
+  } catch (error) {
+    return res.status(400).send({ msg: 'ERROR!', error })
+  }
 }
 
+async function getOneGoal(req: Request, res: Response) {
+  try {
+    const goals = await prisma.goal_Completions.findMany({
+      where: { goalId: req.params.id },
+    })
 
-async function getOneGoal(req: Request, res: Response){
-    try {
-        const goals = await prisma.goal_Completions.findMany({
-            where: {goalId: req.params.id}
-        })
-        
-        return res.status(201).send( goals)
-    } catch (error) {
+    // // biome-ignore lint/correctness/noUnreachable: <explanation>
+    // for (let x = 0; x <= 2; x++) {
+    //   let count = 0
+    //   count = count + 1
+    //   return console.log(x)
+    // }
 
-        return res.status(400).send({msg: "ERROR!", error})
+    console.log(goals.length)
 
-        
-    }
+    return res.status(201).send(goals)
+  } catch (error) {
+    return res.status(400).send({ msg: 'ERROR!', error })
+  }
 }
 
-export default {createGoalsCompletions, getOneGoal, getGoalsCompletions}
+export default { createGoalsCompletions, getOneGoal, getGoalsCompletions }
